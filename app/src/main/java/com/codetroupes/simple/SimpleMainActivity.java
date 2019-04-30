3a1654e27557d56eecb183b559eac244e5c35503
liz <toyk1ng@qq,com>
2019-04-30 20:44:09 +0800

    Initial commit

---
+++ app/src/main/java/com/codetroupes/simple/SimpleMainActivity.java
@@ -0,0 +1,13 @@
+package com.codetroupes.simple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_main);
+    }
+}
